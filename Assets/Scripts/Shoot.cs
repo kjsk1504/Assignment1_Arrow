23774c44b83554b92cbea706980581efd30c05e4
kjsk1504 <154114585+kjsk1504@users.noreply.github.com>
2023-12-28 17:41:50 +0900

    초기 버젼
    pdf 내용 그대로

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -6,23 +6,21 @@ public class Shoot : MonoBehaviour
 {
     public GameObject ArrowPrefab;
     public Transform Arrow_transform;
-    [Range(0, 100)] public float shoot_speed;
+    public float shoot_speed;
 
-    Vector2 LookAt()
+    void LookAt()
     {
         Vector2 mouse_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 dir = new Vector2(mouse_position.x - Arrow_transform.position.x, mouse_position.y - Arrow_transform.position.y);
         Arrow_transform.right = dir;
-        return dir;
     }
 
-    void Fire()
+    private void Fire()
     {
         if (Input.GetMouseButtonDown(0))
         {
             GameObject go = Instantiate(ArrowPrefab, Arrow_transform.position, Arrow_transform.rotation);
-            go.GetComponent<Rigidbody2D>().gravityScale = 0;
-            go.transform.right = LookAt();
+            go.GetComponent<Rigidbody2D>().velocity = Arrow_transform.transform.right * shoot_speed;
         }
     }
 
@@ -30,5 +28,6 @@ public class Shoot : MonoBehaviour
     void Update()
     {
         LookAt();
+        Fire();
     }
 }
